707bba23752a3efcfc085ca733a381c87ba20525
François Michonneau <francois.michonneau@gmail.com>
2021-11-15 10:19:36 +0100

    change site id for matomo

--- assets/js/matomo-analytics.js
+++ assets/js/matomo-analytics.js
@@ -8,7 +8,7 @@ _paq.push(['enableLinkTracking']);
 (function() {
     var u="https://carpentries.matomo.cloud/";
     _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '2']);
+    _paq.push(['setSiteId', '6']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.async=true; g.src='//cdn.matomo.cloud/carpentries.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
 })();
